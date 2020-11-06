0a2fb50a2d431ef731f83607f1133a9ab02aa928
Pierre DAILY <contact@pierredaily.com>
2020-11-06 22:26:24 +0800

    image selection working

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,9 @@
 // Put your application javascript here
+
+$(document).ready(function() {
+    $('#productSelect').change(function() {
+        var variantId = this.value;
+        var selectedImage = $('img.product-display[data-variant="' + variantId +'"]');
+        selectedImage.show().siblings('img.product-display:visible').hide();
+    });
+});
